0c328a94c48b6b06a7d61841e4e9453655d16082
Vilius <vilius.kazlauskas@bentley.com>
2022-04-06 21:27:54 +0300

    Added css file

--- src/content.js
+++ src/content.js
@@ -1,14 +1,5 @@
 import pixelmatch from "pixelmatch";
 
-// const imgFiles = [...document.querySelectorAll(".file[data-file-type='.png']")].map((el) => {
-//   const filePath = el.dataset.tagsearchPath;
-//   return {
-//     old: `https://raw.githubusercontent.com/iTwin/iTwinUI/${baseCommitId}/${filePath}`,
-//     new: `https://raw.githubusercontent.com/iTwin/iTwinUI/${endCommitId}/${filePath}`,
-//   };
-// });
-// console.log(imgFiles);
-
 let observer;
 const observe = () => {
   observer && observer.disconnect();
@@ -31,25 +22,16 @@ const onCompareButtonClick = async (file, baseCommitId, endCommitId, organizatio
   const renderWrapper = file.querySelector(".render-wrapper");
   renderWrapper.innerHTML = "";
   const renderContainer = document.createElement("div");
-  renderContainer.classList.add("render-wrapper");
+  renderContainer.classList.add("ghpric-render-container");
   renderWrapper.appendChild(renderContainer);
-
-  // const renderContainer = file.querySelector(".render-container");
-  renderContainer.style.height = "auto";
-  renderContainer.style.display = "flex";
-  renderContainer.style.flexWrap = "wrap";
-  renderContainer.style.justifyContent = "center";
-  renderContainer.style.gap = "16px";
-  renderContainer.style.padding = "16px";
-  renderContainer.style.backgroundColor = "#f6f8fa";
 
   const filePath = file.dataset.tagsearchPath;
   const oldImage = await getImg(filePath, organization, repo, baseCommitId);
   const newImage = await getImg(filePath, organization, repo, endCommitId);
   const width = Math.max(oldImage.width, newImage.width);
   const height = Math.max(oldImage.height, newImage.height);
-  const oldImgCanvas = createImgCanvas2(oldImage, width, height, "#cf222e", renderContainer);
-  const newImgCanvas = createImgCanvas2(newImage, width, height, "#2da44e", renderContainer);
+  const oldImgCanvas = createImgCanvas2(oldImage, width, height, "before", renderContainer);
+  const newImgCanvas = createImgCanvas2(newImage, width, height, "after", renderContainer);
   renderContainer.appendChild(oldImgCanvas);
   renderContainer.appendChild(newImgCanvas);
 
@@ -58,9 +40,7 @@ const onCompareButtonClick = async (file, baseCommitId, endCommitId, organizatio
   const oldImg = oldImgCanvas.getContext("2d").getImageData(0, 0, oldImgCanvas.width, oldImgCanvas.height);
   const newImg = newImgCanvas.getContext("2d").getImageData(0, 0, newImgCanvas.width, newImgCanvas.height);
   const diffCanvas = document.createElement("canvas");
-  diffCanvas.style.border = "1px solid #d0d7de";
-  diffCanvas.style.flexShrink = "0";
-  diffCanvas.style.maxWidth = "30%";
+  diffCanvas.classList.add("ghpric-canvas", "ghpric-canvas-diff");
   diffCanvas.width = width;
   diffCanvas.height = height;
   diffCanvas.style.cursor = "pointer";
@@ -68,7 +48,7 @@ const onCompareButtonClick = async (file, baseCommitId, endCommitId, organizatio
   const diff = diffContext.createImageData(width, height);
   pixelmatch(oldImg.data, newImg.data, diff.data, width, height, { threshold });
   diffContext.putImageData(diff, 0, 0);
-  addDialogToCanvas(diffCanvas, width, height, "#d0d7de", renderContainer);
+  addDialogToCanvas(diffCanvas, width, height, "diff", renderContainer);
   renderContainer.appendChild(diffCanvas);
 };
 
@@ -82,48 +62,26 @@ const getImg = async (imgPath, organization, repo, commitId) => {
     img.src = `https://raw.githubusercontent.com/${organization}/${repo}/${commitId}/${imgPath}`;
   });
 };
-
-const createImgCanvas = async (imgPath, commitId) => {
-  return new Promise((resolve, reject) => {
-    const imgCanvas = document.createElement("canvas");
-    const imgContext = imgCanvas.getContext("2d");
-    const img = new Image();
-    img.setAttribute("crossOrigin", "");
-    img.onload = () => {
-      imgCanvas.width = img.width;
-      imgCanvas.height = img.height;
-      console.log("img", img.width, img.height);
-      imgContext.drawImage(img, 0, 0);
-      resolve(imgCanvas);
-    };
-    img.src = `https://raw.githubusercontent.com/${organization}/${repo}/${commitId}/${imgPath}`;
-    // imgCanvas.classList.add("render-viewer");
-  });
-};
-
-const createImgCanvas2 = (img, width, height, color, renderContainer) => {
+const createImgCanvas2 = (img, width, height, typePostfix, renderContainer) => {
   const imgCanvas = document.createElement("canvas");
   const imgContext = imgCanvas.getContext("2d");
   imgCanvas.width = width;
   imgCanvas.height = height;
-  imgCanvas.style.flexShrink = "0";
-  imgCanvas.style.maxWidth = "30%";
-  imgCanvas.style.border = `1px solid ${color}`;
-  imgCanvas.style.cursor = "pointer";
+  imgCanvas.classList.add("ghpric-canvas", `ghpric-canvas-${typePostfix}`);
   imgContext.drawImage(img, 0, 0);
-  addDialogToCanvas(imgCanvas, width, height, color, renderContainer);
+  addDialogToCanvas(imgCanvas, width, height, typePostfix, renderContainer);
   return imgCanvas;
 };
 
-const addDialogToCanvas = (imgCanvas, width, height, color, renderContainer) => {
+const addDialogToCanvas = (imgCanvas, width, height, typePostfix, renderContainer) => {
   imgCanvas.onclick = () => {
     const dialog = document.createElement("dialog");
-    dialog.style.border = `1px solid ${color}`;
+    dialog.classList.add("ghpric-dialog", `ghpric-dialog-${typePostfix}`);
     const newCanvas = document.createElement("canvas");
     newCanvas.width = width;
     newCanvas.height = height;
     newCanvas.getContext("2d").drawImage(imgCanvas, 0, 0);
-    newCanvas.style.maxWidth = "100%";
+    newCanvas.classList.add("ghpric-dialog-canvas");
     dialog.appendChild(newCanvas);
     renderContainer.appendChild(dialog);
     dialog.showModal();
